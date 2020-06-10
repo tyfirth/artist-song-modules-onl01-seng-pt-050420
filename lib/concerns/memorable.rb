dfad66eb459e3ad9d40d1e9c4f909ab6e2dc8025
Tyler Firth <tyfirth@gmail.com>
2020-06-10 20:55:34 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -9,12 +9,15 @@ module Memorable
       def count
         self.all.count
       end
+      
     end
       
       module InstanceMethods
+        
         def initialize
           self.class.all << self
         end
+        
       end
       
       
