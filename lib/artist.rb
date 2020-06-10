71da1eff4ac58bcbb87577b74d68c4a93965ca4d
Tyler Firth <tyfirth@gmail.com>
2020-06-10 20:56:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,10 +12,10 @@ class Artist
 
   @@artists = []
 
-  def initialize
-    super
-    @songs = []
-  end
+  # def initialize
+  #   super
+  #   @songs = []
+  # end
 
   # def self.find_by_name(name)
   #   @@artists.detect{|a| a.name == name}
